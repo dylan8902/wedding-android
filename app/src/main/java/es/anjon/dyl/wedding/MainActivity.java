297a9089453f20a32abd8549832ffb02df61dcb1
Dylan Jones <dyl@anjon.es>
2018-03-31 22:30:41 +0100

    Add configurable navigation
    Menu is driven by firebase

--- app/src/main/java/es/anjon/dyl/wedding/MainActivity.java
+++ app/src/main/java/es/anjon/dyl/wedding/MainActivity.java
@@ -23,24 +23,28 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import es.anjon.dyl.wedding.fragments.HomeFragment;
 import es.anjon.dyl.wedding.fragments.PhotosFragment;
 import es.anjon.dyl.wedding.fragments.QuizFragment;
 import es.anjon.dyl.wedding.fragments.TablePlanFragment;
+import es.anjon.dyl.wedding.models.Navigation;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = "MainActivity";
-    private static final String TABLE_PLAN_KEY = "table_plan";
-    private static final String QUIZ_KEY = "quiz";
-    private static final String PHOTOS_KEY = "photos";
-    private static final int TABLE_PLAN_ID = 201;
-    private static final int QUIZ_ID = 203;
-    private static final int PHOTOS_ID = 204;
+    private static final int HOME_ID = 201;
+    private static final int MAP_ID = 202;
+    private static final int TABLE_PLAN_ID = 203;
+    private static final int QUIZ_ID = 204;
+    private static final int PHOTOS_ID = 205;
     private static final LatLng ST_MARYS = new LatLng(51.473842,-3.172077);
     private static final LatLng ST_DAVIDS = new LatLng(51.4605074,-3.1672796);
+    private Navigation mNav = new Navigation();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -56,7 +60,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        final Menu menu = navigation.getMenu();
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        mNav.loadPrefs(prefs);
+        updateNavigation(menu);
 
         final FirebaseAuth auth = FirebaseAuth.getInstance();
         auth.signInAnonymously().addOnCompleteListener(
@@ -72,43 +81,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     }
                 });
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        Menu menu = navigation.getMenu();
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.setPersistenceEnabled(true);
+        firebaseDatabase.getReference(Navigation.KEY).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onDataChange:" + dataSnapshot.getKey());
+                mNav = dataSnapshot.getValue(Navigation.class);
+                mNav.updatePrefs(prefs);
+                updateNavigation(menu);
+            }
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-
-        //TODO move to notification handler
-        prefs.edit().putBoolean(TABLE_PLAN_KEY, true).apply();
-        prefs.edit().putBoolean(QUIZ_KEY, true).apply();
-        prefs.edit().putBoolean(PHOTOS_KEY, true).apply();
-
-        if (prefs.getBoolean(TABLE_PLAN_KEY, false)) {
-            menu.add(Menu.NONE, TABLE_PLAN_ID, Menu.NONE, getString(R.string.title_table_plan))
-                    .setIcon(R.drawable.ic_restaurant_menu_black_24dp);
-        }
-
-        if (prefs.getBoolean(QUIZ_KEY, false)) {
-            menu.add(Menu.NONE, QUIZ_ID, Menu.NONE, getString(R.string.title_quiz))
-                    .setIcon(R.drawable.ic_quiz_black_24dp);
-        }
-
-        if (prefs.getBoolean(PHOTOS_KEY, false)) {
-            menu.add(Menu.NONE, PHOTOS_ID, Menu.NONE, getString(R.string.title_photos))
-                    .setIcon(R.drawable.ic_photos_black_24dp);
-        }
-
-        selectFragment(navigation.getMenu().getItem(0));
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled:", databaseError.toException());
+            }
+        });
     }
 
     private void selectFragment(MenuItem item) {
         Fragment frag = null;
 
         switch (item.getItemId()) {
-            case R.id.navigation_home:
+            case HOME_ID:
                 frag = HomeFragment.newInstance(getString(R.string.title_home));
                 break;
-            case R.id.navigation_map:
+            case MAP_ID:
                 frag = SupportMapFragment.newInstance();
                 ((SupportMapFragment) frag).getMapAsync(this);
                 break;
@@ -137,6 +135,35 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         googleMap.addMarker(new MarkerOptions().position(ST_DAVIDS)
                 .title("The Principal St David's Hotel"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ST_MARYS, 14));
+    }
+
+    /**
+     * Update the navigation menu
+     * @param menu the menu to update
+     */
+    private void updateNavigation(Menu menu) {
+        Log.i(TAG, "updateNavigation:" + mNav.toString());
+        menu.clear();
+        menu.add(Menu.NONE, HOME_ID, Menu.NONE, getString(R.string.title_home))
+                .setIcon(R.drawable.ic_home_black_24dp);
+        menu.add(Menu.NONE, MAP_ID, Menu.NONE, getString(R.string.title_map))
+                .setIcon(R.drawable.ic_map_black_24dp);
+
+        if (mNav.isTablePlan()) {
+            menu.add(Menu.NONE, TABLE_PLAN_ID, Menu.NONE, getString(R.string.title_table_plan))
+                    .setIcon(R.drawable.ic_restaurant_menu_black_24dp);
+        }
+
+        if (mNav.isQuiz()) {
+            menu.add(Menu.NONE, QUIZ_ID, Menu.NONE, getString(R.string.title_quiz))
+                    .setIcon(R.drawable.ic_quiz_black_24dp);
+        }
+
+        if (mNav.isPhotos()) {
+            menu.add(Menu.NONE, PHOTOS_ID, Menu.NONE, getString(R.string.title_photos))
+                    .setIcon(R.drawable.ic_photos_black_24dp);
+        }
+        selectFragment(menu.getItem(0));
     }
 
 }
